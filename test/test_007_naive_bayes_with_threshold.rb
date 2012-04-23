daad9ebe5ee9c70e6bd204b7ab0ae6f8985a11da
Victor Pereira <victor.pereira@adyton.net>
2012-04-23 16:10:29 +0200

    nosense typo

--- test/test_007_naive_bayes_with_threshold.rb
+++ test/test_007_naive_bayes_with_threshold.rb
@@ -4,7 +4,7 @@ class Test007NaiveBayesClassificationWithThreshold < TestBase
 
   before do 
     set_classifier StuffClassifier::Bayes.new("Cats or Dogs",:max_prob=>0.04)
-    train :dog, "Dogs are Awesome, but in Germany they do Auw Auw"
+    train :dog, "Dogs are Awesome, but in Germany they do Wuff wuff"
     train :cat, "Cats are ok, but too independent"
     train :dog, "Dogs in Brasil do Au Au"
     train :cat, "Cats, in another hand do always Miau, Miau"
