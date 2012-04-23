d927709097fc9d43a63ce7e87ac8bf7e86f72e6f
Olivier Gosse-Gardet <olivier.gosse.gardet@gmail.com>
2012-04-23 10:14:34 +0200

    refactored : word_count & category_count into word_list and category_list

--- lib/stuff-classifier/storage.rb
+++ lib/stuff-classifier/storage.rb
@@ -8,22 +8,22 @@ module StuffClassifier
 
     def load_state(classifier)
       if @storage.key? classifier.name
-        _word_count, _category_count,_word_total,_category_total = @storage[classifier.name]
+        _word_list, _category_list, _training_count = @storage[classifier.name]
         classifier.instance_eval do
-          @word_count = _word_count
-          @category_count = _category_count
-
-          @word_total = _word_total
-          @category_total = _category_total
+          @word_list = _word_list
+          @category_list = _category_list
+          @training_count=_training_count
         end
       end
     end
 
     def save_state(classifier)
       name = classifier.name
-      word_count = classifier.instance_variable_get :@word_count
-      category_count = classifier.instance_variable_get :@category_count
-      @storage[name] = [word_count, category_count]
+      word_list = classifier.instance_variable_get :@word_list
+      category_list = classifier.instance_variable_get :@category_list
+      training_count = classifier.instance_variable_get :@training_count
+
+      @storage[name] = [word_list, category_list, training_count]
     end
 
     def purge_state(classifier)
@@ -43,19 +43,21 @@ module StuffClassifier
       end
 
       if @storage.key? classifier.name
-        _word_count, _category_count = @storage[classifier.name]
+        _word_list, _category_list, _training_count = @storage[classifier.name]
         classifier.instance_eval do
-          @word_count = _word_count
-          @category_count = _category_count
+          @word_list = _word_list
+          @category_list = _category_list
+          @training_count=_training_count          
         end
       end
     end
 
     def save_state(classifier)
       name = classifier.name
-      word_count = classifier.instance_variable_get :@word_count
-      category_count = classifier.instance_variable_get :@category_count
-      @storage[name] = [word_count, category_count]
+      word_list = classifier.instance_variable_get :@word_list
+      category_list = classifier.instance_variable_get :@category_list
+      training_count = classifier.instance_variable_get :@training_count
+      @storage[name] = [word_list, category_list, training_count]
       _write_to_file
     end
 
